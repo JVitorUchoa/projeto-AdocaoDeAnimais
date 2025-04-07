eb49b8b6b515984ec6138685acf37a8b27dc5372
JKauacastro <158325760+JKauacastro@users.noreply.github.com>
2025-04-07 17:08:18 -0300

    Tirei o fundo das imagens, coloquei fade-in e out pro botão e pra caixavasia
    Tirei o fundo das imagens, coloquei fade-in e out para o botão e para a caixavasia, aumentei a fonte do titulo pra parecer que a tela esta mais cheia

--- scripts js/utils_index.js
+++ scripts js/utils_index.js
@@ -13,15 +13,24 @@ function rolarParaBaixo() {
 // Seta o intervalo para o window.scrollBy ser ativado
 const intervalo = setInterval(rolarParaBaixo, 8);
 
+const titulo = document.getElementById('titulo');
+const caixa1 = document.getElementById("caixa1");
+const continuar = document.getElementById('continuar');
+
 // Função para verificar a posição do titulo
 function verificarPosicaoTitulo() {
     
     // Verifica se o scroll chegou no fim da pagina e quando chegar faz o fade-in e quando sair fade-out
     if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
         titulo.classList.add('visivel');
+        caixa1.classList.add('visivel');
+        continuar.classList.add('visivel');
     } else {
         titulo.classList.remove('visivel');
+        caixa1.classList.remove('visivel');
+        continuar.classList.remove('visivel');
     }
+
 }
 
 // Verifica a rolagem da pagina
