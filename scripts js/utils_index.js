b5ee489264d0b79cef5e7b30e59a54d54c23c88e
bmenezes680 <beatrizmenezes285@gmail.com>
2025-04-12 08:21:43 -0300

    Foi adicionado block

--- scripts js/utils_index.js
+++ scripts js/utils_index.js
@@ -1,11 +1,11 @@
-// Função para rolar para baixo até o final da pagina
+// Função para rolar para baixo até o final da página
 function rolarParaBaixo() {
-    
-    // Para controlar o scroll até chegar no final da pagina
-    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+    // Para controlar o scroll até chegar no final da página
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
         clearInterval(intervalo);
+        bloquearTela();
     } else {
-        // Define quantos pixeis vao ser escrolados para baixo
+        // Define quantos pixels vão ser rolados para baixo
         window.scrollBy(0, 1.5);
     }
 }
@@ -17,21 +17,33 @@ const titulo = document.getElementById('titulo');
 const caixa1 = document.getElementById("caixa1");
 const continuar = document.getElementById('continuar');
 
-// Função para verificar a posição do titulo
+// Função para verificar a posição do título
 function verificarPosicaoTitulo() {
-    
-    // Verifica se o scroll chegou no fim da pagina e quando chegar faz o fade-in
+    // Verifica se o scroll chegou no fim da página e, quando chegar, faz o fade-in
     if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+     
         titulo.classList.add('visivel');
         caixa1.classList.add('visivel');
         continuar.classList.add('visivel');
     }
+} 
 
+function bloquearTela(){
+      // Mostra a camada de bloqueio
+    document.getElementById("bloqueio").style.display = "block";
 }
 
-// Verifica a rolagem da pagina
+// Verifica a rolagem da página
 window.addEventListener('scroll', verificarPosicaoTitulo);
 
-// Verifica a posição da tela quando inicia a pagina
+// Verifica a posição da tela quando inicia a página
 verificarPosicaoTitulo();
+
+// Função que permite o botão "continuar" desbloquear a tela
+document.getElementById("continuar").addEventListener("click", function() {
+    alert("Você clicou no botão continuar!");
+    //document.getElementById("bloqueio").style.display = "none"; //Comando removido
+});
+
+
 
